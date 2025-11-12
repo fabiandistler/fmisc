903daf04b9f2d9dce0fe749cc9b1962e5e5e1359
Fabian Distler <fabian.distler@gmx.net>
2025-11-12 21:14:49 +0100

    clean

--- R/zzz.R
+++ R/zzz.R
@@ -3,8 +3,3 @@
 #' @useDynLib fmisc, .registration = TRUE
 #' @importFrom Rcpp sourceCpp
 NULL
-
-.onAttach <- function(libname, pkgname) {
-  packageStartupMessage("fmisc: Chunking Algorithm for RAM Management")
-  packageStartupMessage("Use ?process_with_chunks to get started")
-}
